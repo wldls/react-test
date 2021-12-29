ed283ed0055949fe539eda87629391c6d0ea908a
jiin <dslet2343@gmail.com>
2021-12-29 21:14:22 +0900

    간단한 앱 만들며 테스트

--- src/App.js
+++ src/App.js
@@ -1,22 +1,23 @@
-import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
+  const [counter, setCounter] = useState(0);
+  const [disabled, setDidabled] = useState(false);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h3 data-testid="counter">{counter}</h3>
+        <div>
+          <button type="button" data-testid="minus-button" disabled={disabled} onClick={() => setCounter((count) => count - 1)}>-</button>
+          <button type="button" data-testid="plus-button" disabled={disabled} onClick={() => setCounter((count) => count + 1)}>+</button>
+        </div>
+        <div>
+          <button type='button' data-testid="on/off-button" style={{backgroundColor: 'blue'}} onClick={() => {
+            setDidabled((prev) => !prev)
+          }}>on/off</button>
+        </div>
       </header>
     </div>
   );
--- src/App.test.js
+++ src/App.test.js
@@ -1,14 +1,52 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('this counter starts at 0', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+  // screen object를 이용해서 원하는 엘리멘트에 접근(접근할 때 ID로)
+  const counterElement = screen.getByTestId("counter");
+  // id가 counter인 엘리멘트의 텍스트가 0인지 테스트
+  expect(counterElement).toHaveTextContent(0);
+});
 
-  const lintTest = screen.getByRole('button', {
-    name: 'lintTest'
-  })
+test('minus button has correct text', () => {
+  render(<App />);
+  const minusButtonElement = screen.getByTestId('minus-button');
+  expect(minusButtonElement).toHaveTextContent('-');
+});
 
-  expect(lintTest.textContent).toBe('lintTest');
+test('plus button has correct text', () => {
+  render(<App/>);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  expect(plusButtonElement).toHaveTextContent('+');
+});
+
+test('when the + button is pressed, the counter changes to 1', () => {
+  render(<App/>);
+  const buttonElement = screen.getByTestId('plus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(1);
+});
+
+test('when the - button is pressed, the counter changes to -1', () => {
+  render(<App/>);
+  const buttonElement = screen.getByTestId('minus-button');
+  fireEvent.click(buttonElement);
+  const counterElement = screen.getByTestId('counter');
+  expect(counterElement).toHaveTextContent(-1);
+});
+
+test('on/off button has blue color', () => {
+  render(<App />);
+  const buttonElement = screen.getByTestId('on/off-button');
+  expect(buttonElement).toHaveStyle({backgroundColor: 'blue'});
+});
+
+test.only('Prevent the -,+ button from being pressed when the on/off button is clicked', () => {
+  render(<App />);
+  const onOffButtonElement = screen.getByTestId('on/off-button');
+  fireEvent.click(onOffButtonElement);
+  const plusButtonElement = screen.getByTestId('plus-button');
+  expect(plusButtonElement).toBeDisabled();
 });
